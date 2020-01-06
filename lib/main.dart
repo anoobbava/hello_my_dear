de4cc56b72d95d8d9224b8bda128b01cabf686c7
Anoob Bava <anoob.bava@gmail.com>
2020-01-06 23:03:47 +0530

    added background and text colur

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff388E3C),
       body: Center(
         child: Text(
           'Welcome My Dear',
-          style: TextStyle(fontSize: 30.0),
+          style: TextStyle(fontSize: 30.0, color: Colors.white),
         ),
       ),
     );
